4f376a37698bf9ee48f6dc3e8c4b329f23a032cb
Josseline <josseline.coutino29@unach.mx>
2025-06-26 13:16:08 -0600

    cambios

--- RESOURCE/JS/forzarCargaCSS.js
+++ RESOURCE/JS/forzarCargaCSS.js
@@ -5,4 +5,36 @@ window.addEventListener('load', function() {
     if (document.body) {
         document.body.classList.add('loaded');
     }
+}); 
+
+var elementosGlobales = {
+    fechaInput: null
+}
+
+function obtenerFecha(){
+    fecha= new Date();
+    var dia = (fecha.getDate()).toString().padStart(2, '0');
+    var mes = (fecha.getMonth() + 1).toString().padStart(2, '0');
+    var anio = fecha.getFullYear();
+    fechaHoy = dia + '/' + mes + '/' + anio;
+    return fechaHoy;
+}
+function inicializarElementosGlobales() {
+    elementosGlobales.fechaInput = document.getElementById('fecha');
+    if (elementosGlobales.fechaInput) {
+        elementosGlobales.fechaInput.value = obtenerFecha();
+    }
+}
+
+function guardarLocalStorage() {
+    if (elementosGlobales.fechaInput) {
+        localStorage.setItem('valor_fecha', elementosGlobales.fechaInput.value || obtenerFecha());
+    }
+}
+document.addEventListener('DOMContentLoaded', function() {
+    inicializarElementosGlobales(); 
+    var valorFecha = localStorage.getItem('valor_fecha') || obtenerFecha();
+    if (elementosGlobales.fechaElemento) {
+        elementosGlobales.fechaElemento.innerHTML = valorFecha;
+    }
 });
