58c510aa8dd4b25f2c237f8f2fde5961d38a0ab5
Josseline <josseline.coutino29@unach.mx>
2025-06-30 13:36:33 -0600

    TPS Y FECHA

--- RESOURCE/JS/forzarCargaCSS.js
+++ RESOURCE/JS/forzarCargaCSS.js
@@ -6,3 +6,42 @@ window.addEventListener('load', function() {
         document.body.classList.add('loaded');
     }
 });
+
+var elementosGlobales = {
+    fechaInput: null,
+    fechaElemento: null,
+};
+
+function obtenerFecha() {
+    var fecha = new Date();
+    var dia = (fecha.getDate()).toString().padStart(2, '0');
+    var mes = (fecha.getMonth() + 1).toString().padStart(2, '0');
+    var anio = fecha.getFullYear();
+    var fechaHoy = dia + '/' + mes + '/' + anio;
+    return fechaHoy;
+}
+
+function inicializarElementosGlobales() {
+    elementosGlobales.fechaInput = document.getElementById('fecha');
+    elementosGlobales.fechaElemento = document.getElementById('fecha1');
+
+    if (elementosGlobales.fechaInput) {
+        elementosGlobales.fechaInput.value = obtenerFecha();
+    }
+}
+
+function guardarLocalStorage() {
+    if (elementosGlobales.fechaInput) {
+        localStorage.setItem('valor_fecha', elementosGlobales.fechaInput.value || obtenerFecha());
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    inicializarElementosGlobales();
+
+    var valorFecha = localStorage.getItem('valor_fecha') || obtenerFecha();
+
+    if (elementosGlobales.fechaElemento) {
+        elementosGlobales.fechaElemento.innerHTML = valorFecha;
+    }
+});
