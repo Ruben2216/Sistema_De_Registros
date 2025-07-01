bf078b1c2a88f29d0decc0417f2fdb982ff97bbe
Monserrat Garcia Esponda <monserrat.garcia84@unach.mx>
2025-07-01 08:42:56 -0600

    me falto el de la rij

--- RESOURCE/JS/formato_RIJ/pdf_rij.js
+++ RESOURCE/JS/formato_RIJ/pdf_rij.js
@@ -1,4 +1,41 @@
 
+function validarYGenerarPDF() {
+    // Busca los campos de tipo text 
+    const camposTexto = document.querySelectorAll('.campo__control[type="text"]:not([style*="display: none"])'); 
+    let camposIncompletos = [];
+
+    // Validar campos de texto visibles
+    camposTexto.forEach(campo => {
+        if (campo.value.trim() === "") {
+            camposIncompletos.push(campo);
+            campo.classList.add("campo__control--error");
+        } else {
+            campo.classList.remove("campo__control--error");
+        }
+    });
+
+    // Validar radios (1 por cada pregunta)
+    const radios = [
+        "inicio_jornada", "#personal", "salud", "ejercicio", "anomalias", "mantenimiento",
+        "operacion", "riesgo", "incidentes", "informacion_extra", "espejo", "prediccion_peligro",
+        "articulo", "experiencia", "actividades_relevantes", "reglas_vida", "politicas", "colaborador",
+        
+    ];
+    radios.forEach(name => {
+        const seleccionado = document.querySelector(`input[name="${name}"]:checked`);
+        if (!seleccionado) {
+            camposIncompletos.push(name);
+        }
+    });
+
+    if (camposIncompletos.length > 0) {
+        alert("Por favor, complete todos los campos obligatorios.");
+    } else {
+        generarPDF();
+    }
+}
+
+
 async function generarPDF() {
     const { jsPDF } = window.jspdf;
     const doc = new jsPDF(); /*crea pdf vacio*/
